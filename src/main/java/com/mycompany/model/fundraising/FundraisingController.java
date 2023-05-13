265f0ffd31e50769cef88c0db568a4217e9e573e
235843 <235843@edu.p.lodz.pl>
2023-05-13 16:16:30 +0200

    Create fund

--- src/main/java/com/mycompany/model/fundraising/FundraisingController.java
+++ src/main/java/com/mycompany/model/fundraising/FundraisingController.java
@@ -1,20 +1,26 @@
 package com.mycompany.model.fundraising;
 
+import com.mycompany.model.user.UserRepository;
 import com.nimbusds.jose.shaded.json.JSONArray;
 import com.nimbusds.jose.shaded.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.json.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import javax.mail.Multipart;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class FundraisingController {
 
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     FundraisingRepository fundraisingRepository;
 
@@ -24,23 +30,9 @@ public class FundraisingController {
         List<Fundraising> funds = fundraisingRepository.findAllByAvailableIsTrueOrderByFundraisingStart();
         System.out.println("/home");
         JSONArray jsonArray = new JSONArray();
-        for(Fundraising fund: funds)
-        {
-            JSONObject jsonObj = new JSONObject();
-            jsonObj.put("id", fund.getId());
-            jsonObj.put("fundraising_start",fund.getFundraisingStart());
-            jsonObj.put("fundraising_end", fund.getFundraisingEnd());
-            jsonObj.put("title", fund.getTitle());
-            jsonObj.put("collected_money",fund.getCollectedMoney());
-            jsonObj.put("goal", fund.getGoal());
-            jsonObj.put("image", fund.getImage());
-            jsonObj.put("owner_name", fund.getOwner().getName());
-            jsonObj.put("owner_surname", fund.getOwner().getSurname());
-
-            jsonArray.add(jsonObj);
-
+        for(Fundraising fund: funds) {
+            jsonArray.add(fundraisingToJSON(fund));
         }
-
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("array", jsonArray);
@@ -50,6 +42,55 @@ public class FundraisingController {
     }
 
 
+    @GetMapping("/fundraising")
+    @ResponseBody
+    public JSONObject getFundraising(@RequestParam(name = "id")Long id){
+        Optional<Fundraising> optionalFundraising = fundraisingRepository.findById(id);
+        if(optionalFundraising.isPresent()) {
+            return fundraisingToJSON(optionalFundraising.get());
+        }
+        return new JSONObject();
+    }
 
+    @GetMapping("/createFundraising")
+    @ResponseBody
+    public JSONObject createFundraising(Authentication authentication){
+        Fundraising fundraising = jsonToFund(new org.json.JSONObject(authentication.getPrincipal()), authentication.getName());
+        fundraisingRepository.save(fundraising);
+
+        return fundraisingToJSON(fundraising);
+    }
+
+    public JSONObject fundraisingToJSON(Fundraising fund){
+        JSONObject jsonObj = new JSONObject();
+
+        jsonObj.put("id", fund.getId());
+        jsonObj.put("fundraising_start",fund.getFundraisingStart());
+        jsonObj.put("fundraising_end", fund.getFundraisingEnd());
+        jsonObj.put("title", fund.getTitle());
+        jsonObj.put("collected_money",fund.getCollectedMoney());
+        jsonObj.put("goal", fund.getGoal());
+        jsonObj.put("image", fund.getImage());
+        jsonObj.put("owner_name", fund.getOwner().getName());
+        jsonObj.put("owner_surname", fund.getOwner().getSurname());
+
+        return jsonObj;
+    }
+
+    public Fundraising jsonToFund(org.json.JSONObject jsonObject, String ownerName){
+        Fundraising fundraising = new Fundraising();
+        fundraising.setFundraisingStart(new Date(jsonObject.getJSONObject("attributes").getString("fundraising_start")));
+        fundraising.setFundraisingEnd(new Date(jsonObject.getJSONObject("attributes").getString("fundraising_end")));
+        fundraising.setTitle(jsonObject.getJSONObject("attributes").getString("title"));
+        fundraising.setGoal(Integer.parseInt(jsonObject.getJSONObject("attributes").getString("goal")));
+        fundraising.setDescription(jsonObject.getJSONObject("attributes").getString("description"));
+        fundraising.setCollectedMoney(0);
+        fundraising.setAvailable(false);
+        fundraising.setOwner(userRepository.getUserByEmail(ownerName));
+        //fundraising.setImage(jsonObject.getJSONObject("attributes").getString("image").getBytes());
+
+        return fundraising;
+    }
 
 }
+
--- src/main/java/com/mycompany/model/fundraising/FundraisingRepository.java
+++ src/main/java/com/mycompany/model/fundraising/FundraisingRepository.java
@@ -4,9 +4,11 @@ import com.mycompany.model.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FundraisingRepository extends CrudRepository<Fundraising, Long> {
 
     List<Fundraising> findAllByOwnerAndAvailableIsTrueOrderByCollectedMoney(User user);
     List<Fundraising> findAllByAvailableIsTrueOrderByFundraisingStart();
+    Optional<Fundraising> findById(Long id);
 }
