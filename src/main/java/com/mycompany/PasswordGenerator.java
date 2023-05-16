774bef520e8ef82aa52ed1b7fac9aa2839d5cb3d
LAPTOP-A84BPH30\48793 <235909@edu.p.lodz.pl>
2023-05-16 15:11:24 +0200

    add register and funds search

--- src/main/java/com/mycompany/PasswordGenerator.java
+++ src/main/java/com/mycompany/PasswordGenerator.java
@@ -3,6 +3,11 @@ package com.mycompany;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
+
+    public String changePassword(String passwordToChange){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(passwordToChange);
+    }
     public static void main(String[] args){
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         String rawPassword = "test";
--- src/main/java/com/mycompany/model/user/UserService.java
+++ src/main/java/com/mycompany/model/user/UserService.java
@@ -1,19 +1,39 @@
 package com.mycompany.model.user;
 
+import com.mycompany.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.swing.text.html.parser.Entity;
 
 @Service
 @Transactional
 public class UserService {
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepository;
 
-//    public void updateAuthenticationType(String username, String oauth2ClientName) {
-//        Resource.AuthenticationType authType = Resource.AuthenticationType.valueOf(oauth2ClientName.toUpperCase());
-//        repo.updateAuthenticationType(username, authType);
-//    }
+    public User registerUser(String name, String email, String password){
+        User user = new User();
+        user.setEmail(email);
+        user.setName(name);
+
+       PasswordGenerator passwordGenerator = new PasswordGenerator();
+       String encodedPassword = passwordGenerator.changePassword(password);
+
+       user.setPassword(encodedPassword);
+       user.setBlocked(false);
+       user.setLoginType(LoginType.BASIC);
+       user.setRole(UserRole.USER);
+
+       userRepository.save(user);
+
+        return userRepository.getUserByEmail(email);
+    }
 }
